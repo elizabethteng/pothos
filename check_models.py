314265cac1024879d6b643b3366686af3891e3c9
Elizabeth Teng <eteng@haverford.edu>
2020-02-10 12:16:04 -0500

    print bounds of finished models

--- check_models.py
+++ check_models.py
@@ -4,6 +4,21 @@ import numpy as np
 import os
 
 dictionary=np.load("./etgrid/et_dictionary.npy")
+
+finished=[]
+
 for i in range(len(dictionary)):
     if dictionary[i]['filename'] in os.listdir("./etgrid/models"):
-        print(i)
+        finished.append(i)
+
+report=[]
+report.append(finished[0])
+
+for i in range(1,len(finished)):
+    if int(finished[i])!=int(finished[i-1])+1:
+        report.append(finished[i-1])
+        report.append(finished[i])
+report.append(finished[-1])
+
+for i in range(int(len(report)/2)):
+    print(report[2*i],report[2*i+1])
