527cbd69aad82bf633db798b7c12b3e31f2bfc6d
Elizabeth Teng <eteng@haverford.edu>
2020-02-22 13:03:41 -0500

    changes

--- errorgrid.py
+++ errorgrid.py
@@ -4,30 +4,26 @@ import numpy as np
 import os
 import pickle
 from time import time
-import lhsmdu
 import pdspy.modeling as modeling
 import pdspy.dust as dust
+import argparse
 
-desc='''run new model grid with transformations'''
+
+parser=argparse.ArgumentParser()
+parser.add_argument("--threads", help="number of threads",type=int)
+parser.add_argument("--start",help="starting :number",type=int)
+parser.add_argument("--end",help="ending number",type=int)
 
 param_names = ["Tstar","logLstar","logMdisk","logRdisk","h0","logRin",\
           "gamma","beta","logMenv","logRenv","fcav","ksi","logamax","p","incl"]
 
+
 def run_yso_model( Tstar=None, logL_star=None, logM_disk=None, logR_disk=None, h_0=None, \
                   logR_in=None, gamma=None, beta=None, logM_env=None, logR_env=None, \
-                  f_cav=None, ksi=None, loga_max=None, p=None, incl=None):
+                  f_cav=None, ksi=None, loga_max=None, p=None, incl=None, filename=None):
     
-    params = [Tstar,logL_star,logM_disk,logR_disk,h_0,logR_in,\
-          gamma,beta,logM_env,logR_env,f_cav,ksi,loga_max,p,incl]
-    
-    filename=""
-    for i in range(len(param_names)):
-        filename+=param_names[i]+"_"
-        filename+=str(params[i])+"_"
-    filename=filename[:-1]
-    filename+=".hdf5"
     print("starting on "+filename)
-    
+  
     # Set up the dust properties.
     dust_gen = dust.DustGenerator(dust.__path__[0]+"/data/diana_wice.hdf5")
     ddust = dust_gen(10.**loga_max / 1e4, p)
@@ -46,7 +42,7 @@ def run_yso_model( Tstar=None, logL_star=None, logM_disk=None, logR_disk=None, h
     model.add_star(luminosity=10.**logL_star, temperature=Tstar)
     model.set_spherical_grid(10.**logR_in, 10.**logR_env, 100, 101, 2, \
             code="radmc3d")
-    model.add_pringle_disk(mass=10.**logM_disk, rmin=10.**logR_in, \
+    model.add_pringle_disk(mass=10.**logM_disk, rmin=10.**logR_in,\
             rmax=10.**logR_disk, plrho=alpha, h0=h_0, plh=beta, dust=ddust)
     model.add_ulrich_envelope(mass=10.**logM_env, rmin=10.**logR_in, \
             rmax=10.**logR_env, cavpl=ksi, cavrfact=f_cav, dust=edust)
@@ -54,29 +50,26 @@ def run_yso_model( Tstar=None, logL_star=None, logM_disk=None, logR_disk=None, h
 
     # Run the thermal simulation
     model.run_thermal(code="radmc3d", nphot=1e6, \
-            modified_random_walk=True, verbose=False, setthreads=15, \
+            modified_random_walk=True, verbose=False, setthreads=parser.parse_args().threads, \
             timelimit=10800)
     
     # Run the SED
-    t2=time()
     model.set_camera_wavelength(np.logspace(-1.,4.,500))
-    model.run_sed(name="SED", nphot=1e6, loadlambda=True, incl=incl, \
+    model.run_sed(name="SED", nphot=1e5, loadlambda=True, incl=incl, \
             pa=0., dpc=140., code="radmc3d", camera_scatsrc_allfreq=True, \
-            verbose=False, setthreads=15)
-
-    print("finished running SED for "+filename[0:40]+"... in %0.3fs" % (time() - t2))
+            verbose=False, setthreads=parser.parse_args().threads)
     
     # Write out the file.
-    model.write_yso("./etgrid/filename/"+filename)
+    model.write_yso("./../etgrid/models/"+filename)
 
-with open ('./etgrid/etgrid_coords_bypoint_orig.txt', 'rb') as fp:
-    coords = pickle.load(fp)    
+dictionary=np.load("./../etgrid/et_dictionary.npy")
     
-for i in range(len(coords)):
+for i in range(parser.parse_args().start,parser.parse_args().end):
     t0=time()
-    run_yso_model(Tstar=coords[i][0], logL_star=coords[i][1], logM_disk=coords[i][2], \
-                  logR_disk=coords[i][3], h_0=coords[i][4], logR_in=coords[i][5],\
-                  gamma=coords[i][6], beta=coords[i][7], logM_env=coords[i][8], \
-                  logR_env=coords[i][9], f_cav=coords[i][10], ksi=coords[i][11], \
-                  loga_max=coords[i][12], p=coords[i][13], incl=coords[i][14])
+    point=dictionary[i]
+    run_yso_model( Tstar=point["Tstar"], logL_star=point["logLstar"], logM_disk=point["logMdisk"], \
+                  logR_disk=point["logRdisk"], h_0=point["h0"], logR_in=point["logRin"], gamma=point["gamma"], \
+                  beta=point["beta"], logM_env=point["logMenv"], logR_env=point['logRenv'], \
+                  f_cav=point['fcav'], ksi=point['ksi'], loga_max=point['logamax'], p=point['p'], \
+                  incl=point['incl'], filename=point['filename'])
     print("finished running SED #"+str(i)+" in %0.3fs" % (time() - t0))
